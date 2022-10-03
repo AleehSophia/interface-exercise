66906975e22d3382f6fba41cca4ae9be47a965f7
AleehSophia <alessia.casado@gmail.com>
2022-10-03 12:08:28 -0300

    :sparkles:feat: OnlinePaymentService interface

---
+++ src/model/services/OnlinePaymentService.java
@@ -0,0 +1,7 @@
+package model.services;
+
+public interface OnlinePaymentService {
+
+    double paymenteFee(double amount);
+    double interest(double amount, int months);
+}
---
+++ src/model/services/PaypalService.java
@@ -0,0 +1,2 @@
+package model.entities.model.services;public interface PaypalService {
+}
