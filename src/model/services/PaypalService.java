ec465199ce88935171b6f8750a50c65ecd3b8e85
AleehSophia <alessia.casado@gmail.com>
2022-10-03 12:13:46 -0300

    sparkles:feat: PaypalService

--- src/model/services/PaypalService.java
+++ src/model/services/PaypalService.java
@@ -1,2 +1,17 @@
-package model.entities.model.services;public interface PaypalService {
+package model.services;
+
+public class PaypalService implements OnlinePaymentService {
+
+    private static final double PAYMENT_FEE = 0.02;
+    private static final double MONTHLY_INTEREST = 0.01;
+
+    @Override
+    public double paymenteFee(double amount) {
+        return amount * PAYMENT_FEE;
+    }
+
+    @Override
+    public double interest(double amount, int months) {
+        return amount * months * MONTHLY_INTEREST;
+    }
 }
